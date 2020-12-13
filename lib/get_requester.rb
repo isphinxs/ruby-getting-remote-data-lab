efee3c41a51b9582eb90ef8623d7e87b73db65c3
Samantha Ostrowski <isphinxs@hotmail.com>
2020-12-12 20:30:33 -0500

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,11 +5,11 @@ require "json"
 
 class GetRequester
     def initialize(url)
-        @@url = url
+        @url = url
     end
 
     def get_response_body
-        uri = URI.parse(@@url)
+        uri = URI.parse(@url)
         response = Net::HTTP.get_response(uri)
         response.body
     end
